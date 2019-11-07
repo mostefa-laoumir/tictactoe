d82d87e1478862db049829dc498fa6c7ffe9c168
Laoumir Mustapha Abdelkader <21nounou21@gmail.com>
2019-11-07 21:22:59 +0100

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -10,11 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener {
-        var yob:Int = editText.text.toString().toInt()
-        yob = 2019 - yob
-            text.text= yob.toString()
+
+
+    }
+    protected fun clk(view:View){
+        val buSelected = view as Button
+        var cellID =0
+        when(buSelected.id){
+            R.id.a1 -> cellID=1
+            R.id.a2 -> cellID=2
+            R.id.a3 -> cellID=3
+            R.id.b1 -> cellID=4
+            R.id.b2 -> cellID=5
+            R.id.b3 -> cellID=6
+            R.id.c1 -> cellID=7
+            R.id.c2 -> cellID=8
+            R.id.c3 -> cellID=9
 
         }
+        Toast.makeText(this,"ID:"+cellID,Toast.LENGTH_LONG).show()
     }
 }
